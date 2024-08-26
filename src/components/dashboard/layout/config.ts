d060a7223f3f759e2903540b19695cd2635bc226
mian-wahab <88232489+mian-wahab@users.noreply.github.com>
2024-08-27 02:40:44 +0500

    memberchanges

--- src/components/dashboard/layout/config.ts
+++ src/components/dashboard/layout/config.ts
@@ -3,7 +3,7 @@ import { paths } from '@/paths';
 
 export const navItems = [
   // { key: 'overview', title: 'Overview', href: paths.dashboard.overview, icon: 'chart-pie' },
-  { key: 'vendors', title: 'Vendors', href: paths.dashboard.vendors, icon: 'users' },
+  { key: 'vendors', title: 'Members', href: paths.dashboard.vendors, icon: 'users' },
   // { key: 'integrations', title: 'Integrations', href: paths.dashboard.integrations, icon: 'plugs-connected' },
   // { key: 'settings', title: 'Settings', href: paths.dashboard.settings, icon: 'gear-six' },
   { key: 'ftp', title: 'FTP', href: paths.dashboard.ftp, icon: 'user' },
@@ -11,3 +11,4 @@ export const navItems = [
   // { key: 'error', title: 'Error', href: paths.errors.notFound, icon: 'x-square' },
   // {key : 'vendor', title: 'Vendor', href: paths.dashboard.vendor, icon: 'user'}
 ] satisfies NavItemConfig[];
+ 
